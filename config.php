c3e829fa741f7488bcfd7564f448b6ec04f978f1
Moses Tenai <60973092+mosestenai@users.noreply.github.com>
2022-01-16 09:54:03 +0300

    Update config.php

--- config.php
+++ config.php
@@ -6,11 +6,11 @@ $username = "";
 $email    = "";
 $errors = array(); 
 $minpassword = 7;
-$host="ec2-52-71-161-140.compute-1.amazonaws.com";
-$user="glgofglsnqmphi";
-$password="129a4844cea6870ce86ab2b9aabb5ad9cc03045726f5ce9beee146219c441f93";
-$dbname="dd8so82sd2ei0b";
-$port="5432";
+$host="";
+$user="";
+$password="";
+$dbname="";
+$port="";
  
 try{
 $db = new PDO("pgsql:host=$host;dbname=$dbname;port=$port",$user,$password);
